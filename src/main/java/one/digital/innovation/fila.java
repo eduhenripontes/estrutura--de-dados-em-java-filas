38ea6eb904ddadbab1c1eb84528efb4185bb1b65
eduhenripontes <edu.henri@gmail.com>
2022-01-03 19:44:31 -0300

    Implementacao da Classe Fila

---
+++ src/main/java/one/digital/innovation/fila.java
@@ -0,0 +1,75 @@
+package one.digital.innovation;
+
+public class fila {
+
+    private No refNoEntradaFila;
+
+    public fila() {
+        this.refNoEntradaFila = null;
+    }
+
+    public void enqueue(No novoNo) {
+        novoNo.setRefNo(refNoEntradaFila);
+        refNoEntradaFila = novoNo;
+    }
+
+    public No first() {
+        if (!this.isEmpty()) {
+            No primeiroNo = refNoEntradaFila;
+            while (true) {
+                if (primeiroNo.getRefNo() != null) {
+                } else {
+                    break;
+                }
+            }
+        }
+        return null;
+    }
+
+    public No dequeue() {
+        if (!this.isEmpty()) {
+            No primeiroNo = refNoEntradaFila;
+            No noAuxiliar = refNoEntradaFila;
+            while (true) {
+                if (primeiroNo.getRefNo() != null) {
+                    noAuxiliar = primeiroNo;
+                    primeiroNo = primeiroNo.getRefNo();
+                } else {
+                    noAuxiliar.setRefNo(null);
+                    break;
+                }
+            }
+            return primeiroNo;
+        }
+        return null;
+    }
+
+    public boolean isEmpty() {
+        return refNoEntradaFila == null ? true : false;
+
+    }
+
+    @Override
+    public String toString() {
+        String stringRetorno = "";
+        No noAuxiliar = refNoEntradaFila;
+
+        if (refNoEntradaFila != null) {
+            while (true) {
+                stringRetorno += "[No{Object= " + noAuxiliar.getObject() + "}]--->";
+                if (noAuxiliar.getRefNo() != null) {
+                    noAuxiliar = noAuxiliar.getRefNo();
+                } else {
+                    stringRetorno += "null";
+                    break;
+                }
+            }
+
+        } else {
+            stringRetorno = null;
+        }
+
+        return stringRetorno;
+    }
+
+}
